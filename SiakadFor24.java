eff62238bb7ac1a98ba337e15facae4be2cae5cd
Wahdaapf <wahdaadella.ba@gmail.com>
2024-10-09 05:10:24 +0200

    feat: hitung yang lulus

--- SiakadFor24.java
+++ SiakadFor24.java
@@ -4,20 +4,28 @@ public class SiakadFor24 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        double nilai, tertinggi = 0.0, terendah = 100.0;
+        double nilai, tertinggi = 0.0, terendah = 100.0, batasKelulusan = 60.0;
+        int lulus = 0, tidakLulus = 0;
 
-        for(int i = 1; i <= 10; i++) {
+        for (int i = 1; i <= 10; i++) {
             System.out.printf("Masukkan Nilai Mahasiswa ke-%s : ", i);
             nilai = sc.nextDouble();
-            if(nilai > tertinggi) {
+            if (nilai > tertinggi) {
                 tertinggi = nilai;
             }
-            if(nilai < terendah) {
+            if (nilai < terendah) {
                 terendah = nilai;
+            }
+            if (nilai >= batasKelulusan) {
+                lulus++;
+            } else {
+                tidakLulus++;
             }
         }
 
-        System.out.println("Nilai Tertinggi\t:" + tertinggi);
-        System.out.println("Nilai Terendah\t:" + terendah);
+        System.out.println("Nilai Tertinggi\t\t: " + tertinggi);
+        System.out.println("Nilai Terendah\t\t: " + terendah);
+        System.out.println("Jumlah Mahasiswa Lulus\t\t: " + lulus);
+        System.out.println("Jumlah Mahasiswa Tidak Lulus\t: " + tidakLulus);
     }
 }
